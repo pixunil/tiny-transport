f49c9c7f8961bffff01b614cbf0fb9b5772f4079
Jakob Stigloher <pixunil@gmx.de>
2019-04-19 18:45:48 +0200

    Refactoring into classes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-function resizeDependent(callback) {
-    callback();
-    addEventListener("resize", callback);
-}
-
 function loadSource(url) {
     return new Promise((resolve, reject) => {
         let request = new XMLHttpRequest;
@@ -27,151 +22,188 @@ function loadSource(url) {
     });
 }
 
-function resizeCanvas(canvas, gl) {
-    canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-    gl.viewport(0, 0, canvas.width, canvas.height);
-}
+class ProgramInfo {
+    async setUp(gl) {
+        this.gl = gl;
 
-async function loadShader(gl, type, source) {
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
+        await this.loadProgram();
+        this.fetchLocations();
+    }
 
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        const log = gl.getShaderInfoLog(shader);
-        gl.deleteShader(shader);
-        throw {
-            type: type,
-            source: source,
-            log: log,
+    async loadProgram() {
+        const shaders = [
+            this.loadShader(this.gl.VERTEX_SHADER, await sources.vertex),
+            this.loadShader(this.gl.FRAGMENT_SHADER, await sources.fragment),
+        ];
+
+        this.program = this.gl.createProgram();
+        for (const shader of shaders) {
+            this.gl.attachShader(this.program, await shader);
+        }
+        this.gl.linkProgram(this.program);
+
+        if (!this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS)) {
+            const log = this.gl.getProgramInfoLog(this.program);
+            this.gl.deleteProgram(this.program);
+            throw {
+                shaders: shaders,
+                log: log,
+            };
+        }
+    }
+
+    async loadShader(type, source) {
+        const shader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            const log = this.gl.getShaderInfoLog(shader);
+            this.gl.deleteShader(shader);
+            throw {
+                type: type,
+                source: source,
+                log: log,
+            };
+        }
+
+        return shader;
+    }
+
+    fetchLocations() {
+        this.uniformLocations = {
+            model: this.gl.getUniformLocation(this.program, "u_model"),
+            view: this.gl.getUniformLocation(this.program, "u_view"),
+        };
+
+        this.attributeLocations = {
+            position: this.gl.getAttribLocation(this.program, "a_position"),
+            center: this.gl.getAttribLocation(this.program, "a_center"),
         };
     }
 
-    return shader;
+    bind(data) {
+        this.gl.useProgram(this.program);
+
+        this.gl.uniformMatrix4fv(this.uniformLocations.model, false, data.matrices.model);
+        this.gl.uniformMatrix4fv(this.uniformLocations.view, false, data.matrices.view);
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, data.buffers.position);
+        this.gl.vertexAttribPointer(
+            this.attributeLocations.position,
+            2, this.gl.FLOAT,
+            false, 0, 0);
+        this.gl.enableVertexAttribArray(this.attributeLocations.position);
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, data.buffers.center);
+        this.gl.vertexAttribPointer(
+            this.attributeLocations.center,
+            2, this.gl.FLOAT,
+            false, 0, 0);
+        this.gl.enableVertexAttribArray(this.attributeLocations.center);
+    }
 }
 
-async function initProgram(gl) {
-    const shaders = [
-        loadShader(gl, gl.VERTEX_SHADER, await sources.vertex),
-        loadShader(gl, gl.FRAGMENT_SHADER, await sources.fragment),
-    ];
-
-    const program = gl.createProgram();
-    for (const shader of shaders) {
-        gl.attachShader(program, await shader);
+class ShaderData {
+    async setUp(gl) {
+        this.canvas = gl.canvas;
+        this.initMatrices();
+        await this.initBuffers(gl);
     }
-    gl.linkProgram(program);
 
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        const log = gl.getProgramInfoLog(program);
-        gl.deleteProgram(program);
-        throw {
-            shaders: shaders,
-            log: log,
+    initMatrices() {
+        this.matrices = {
+            model: new Float32Array([
+                1000.0, 0.0, 0.0, 0.0,
+                0.0, 2000.0, 0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0,
+                -12500.0, -104540.0, 0.0, 1.0,
+            ]),
+            view: new Float32Array([
+                2.0 / this.canvas.width, 0.0, 0.0, 0.0,
+                0.0, 2.0 / this.canvas.height, 0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0,
+                -1.0, -1.0, 0.0, 1.0,
+            ]),
         };
     }
 
-    return {
-        program: program,
-        uniforms: {
-            model: gl.getUniformLocation(program, "u_model"),
-            view: gl.getUniformLocation(program, "u_view"),
-        },
-        attributes: {
-            position: gl.getAttribLocation(program, "a_position"),
-            center: gl.getAttribLocation(program, "a_center"),
-        },
-    };
+    async initBuffers(gl) {
+        this.buffers = {};
+
+        const data = JSON.parse(await sources.data);
+        this.length = 3 * data.length;
+
+        let positions = [];
+        let centers = [];
+
+        for (let stop of data) {
+            positions.push(
+                stop.lon - 10.0, stop.lat - 10.0,
+                stop.lon - 10.0, stop.lat + 30.0,
+                stop.lon + 30.0, stop.lat - 10.0,
+            );
+            centers.push(
+                stop.lon, stop.lat,
+                stop.lon, stop.lat,
+                stop.lon, stop.lat,
+            );
+        }
+
+        this.buffers.position = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.position);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+        this.buffers.center = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.center);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(centers), gl.STATIC_DRAW);
+    }
 }
 
-async function initBuffers(gl) {
-    const data = JSON.parse(await sources.data);
-    let positions = [];
-    let centers = [];
+class Controller {
+    async setUp(gl) {
+        this.gl = gl;
+        this.initializeCanvas();
 
-    for (let stop of data) {
-        positions.push(
-            stop.lon - 10.0, stop.lat - 10.0,
-            stop.lon - 10.0, stop.lat + 30.0,
-            stop.lon + 30.0, stop.lat - 10.0,
-        );
-        centers.push(
-            stop.lon, stop.lat,
-            stop.lon, stop.lat,
-            stop.lon, stop.lat,
-        );
+        this.programInfo = new ProgramInfo();
+        this.shaderData = new ShaderData();
+
+        await Promise.all([
+            this.programInfo.setUp(this.gl),
+            this.shaderData.setUp(this.gl),
+        ]);
+        this.draw();
+        addEventListener("resize", () => this.draw());
     }
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    initializeCanvas() {
+        this.resizeCanvas();
+        addEventListener("resize", () => this.resizeCanvas());
+    }
 
-    const centerBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, centerBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(centers), gl.STATIC_DRAW);
+    resizeCanvas() {
+        const canvas = this.gl.canvas;
+        canvas.width = document.body.clientWidth;
+        canvas.height = document.body.clientHeight;
+        this.gl.viewport(0, 0, canvas.width, canvas.height);
+        this.clear();
+    }
 
-    return {
-        length: positions.length / 2,
-        position: positionBuffer,
-        center: centerBuffer,
-    };
-}
+    clear() {
+        this.gl.clearColor(0.85, 0.9, 0.9, 1.0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    }
 
-async function setUp(gl) {
-    let programInfo = initProgram(gl);
-    let buffers = initBuffers(gl);
+    draw() {
+        this.programInfo.bind(this.shaderData);
 
-    programInfo = await programInfo;
-    buffers = await buffers;
-    resizeDependent(() => draw(gl, programInfo, buffers));
-}
+        this.gl.disable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.BLEND);
+        this.gl.blendFuncSeparate(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA, this.gl.ZERO, this.gl.ONE);
 
-function clear(gl) {
-    gl.clearColor(0.85, 0.9, 0.9, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-}
-
-function draw(gl, programInfo, buffers) {
-    gl.useProgram(programInfo.program);
-
-    const model = new Float32Array([
-        1000.0, 0.0, 0.0, 0.0,
-        0.0, 2000.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        -12500.0, -104540.0, 0.0, 1.0,
-    ]);
-
-    const view = new Float32Array([
-        2.0 / gl.canvas.width, 0.0, 0.0, 0.0,
-        0.0, 2.0 / gl.canvas.height, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        -1.0, -1.0, 0.0, 1.0,
-    ]);
-
-    gl.uniformMatrix4fv(programInfo.uniforms.model, false, model);
-    gl.uniformMatrix4fv(programInfo.uniforms.view, false, view);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-    gl.vertexAttribPointer(
-        programInfo.attributes.position,
-        2, gl.FLOAT,
-        false, 0, 0);
-    gl.enableVertexAttribArray(programInfo.attributes.position);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.center);
-    gl.vertexAttribPointer(
-        programInfo.attributes.center,
-        2, gl.FLOAT,
-        false, 0, 0);
-    gl.enableVertexAttribArray(programInfo.attributes.center);
-
-    gl.disable(gl.DEPTH_TEST);
-    gl.enable(gl.BLEND);
-    gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ZERO, gl.ONE);
-
-    clear(gl);
-    gl.drawArrays(gl.TRIANGLES, 0, buffers.length);
+        this.clear();
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, this.shaderData.length);
+    }
 }
 
 const sources = {
@@ -180,11 +212,10 @@ const sources = {
     data: loadSource("../data/vbb.json"),
 };
 
+const controller = new Controller();
+
 addEventListener("load", () => {
     const canvas = document.querySelector("canvas");
     const gl = canvas.getContext("webgl", {alpha: false});
-    resizeDependent(() => resizeCanvas(canvas, gl));
-    clear(gl);
-
-    setUp(gl).catch(error => console.error(error));
+    controller.setUp(gl).catch(error => console.error(error));
 });
