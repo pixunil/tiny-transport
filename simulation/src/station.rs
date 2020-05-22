d00cecc97d8b151895ab502cd6a8071d24aad415
Jakob Stigloher <pixunil@posteo.eu>
2020-05-22 11:40:45 +0200

    Replace old station! macro in simulation tests

--- simulation/src/station.rs
+++ simulation/src/station.rs
@@ -16,7 +16,7 @@ impl Station {
     }
 
     pub fn contains(&self, position: Point2<f32>) -> bool {
-        na::distance(&self.position, &position) <= 90.0
+        na::distance(&self.position, &position) <= 45.0
     }
 
     pub fn fill_vertice_buffer(&self, buffer: &mut Vec<f32>) {
@@ -48,42 +48,50 @@ mod tests {
     use approx::assert_relative_eq;
 
     use super::*;
+    use crate::fixtures::*;
 
-    #[macro_export]
-    macro_rules! station {
-        (main_station) => {
-            Station::new(Point2::new(200.0, 100.0), "Main Station".to_string())
-        };
+    #[test]
+    fn test_getters() {
+        let station = stations::hauptbahnhof();
+        assert_eq!(station.name(), "Hauptbahnhof");
     }
 
     #[test]
-    fn test_station_contains_friedrich_street() {
-        let station = station!(main_station);
-        assert!(station.contains(Point2::new(200.0, 100.0)));
+    fn test_station_contains_center() {
+        let station = stations::hauptbahnhof();
+        assert!(station.contains(Point2::new(-1385.0, -1812.0)));
     }
 
     #[test]
     fn test_station_contains_border() {
-        let station = station!(main_station);
-        assert!(station.contains(Point2::new(205.0, 100.0)));
-        assert!(station.contains(Point2::new(200.0, 95.0)));
-        assert!(station.contains(Point2::new(195.0, 100.0)));
-        assert!(station.contains(Point2::new(200.0, 105.0)));
+        let station = stations::hauptbahnhof();
+        for &position in &[
+            Point2::new(-1340.0, -1812.0),
+            Point2::new(-1385.0, -1768.0),
+            Point2::new(-1430.0, -1812.0),
+            Point2::new(-1385.0, -1857.0),
+        ] {
+            assert!(station.contains(position));
+        }
     }
 
     #[test]
     fn test_station_excludes_outside() {
-        let station = station!(main_station);
-        assert!(!station.contains(Point2::new(110.0, 10.0)));
-        assert!(!station.contains(Point2::new(290.0, 190.0)));
-        assert!(!station.contains(Point2::new(290.5, 100.0)));
+        let station = stations::hauptbahnhof();
+        for &position in &[
+            Point2::new(-1340.0, -1768.0),
+            Point2::new(-1335.0, -1812.0),
+            Point2::new(-1385.0, -1763.0),
+        ] {
+            assert!(!station.contains(position));
+        }
     }
 
     #[test]
     fn test_station_vertices() {
-        let station = station!(main_station);
+        let station = stations::hauptbahnhof();
         let mut buffer = Vec::new();
         station.fill_vertice_buffer(&mut buffer);
-        assert_relative_eq!(*buffer, [200.0, 100.0])
+        assert_relative_eq!(*buffer, [-1385.0, -1812.0])
     }
 }
