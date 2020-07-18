b8a4470f323266b2991bd6e6c64e7815a88858fb
Jakob Stigloher <pixunil@posteo.eu>
2020-07-18 21:56:19 +0200

    Run command after import by separating it with -- This provides more flexibility for arguments and makes things a bit more explicit

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,8 +1,8 @@
 use std::convert::TryInto;
 use std::error::Error;
+use std::fmt;
 use std::fs::{self, File};
 use std::io::{self, Write};
-use std::ops::Deref;
 use std::path::PathBuf;
 
 use chrono::NaiveDate;
@@ -26,41 +26,27 @@ fn validate_date(value: String) -> Result<(), String> {
         .map_err(|error| format!("{}, it must be in the format yyyy-mm-dd", error))
 }
 
-enum ImportedDatasetRef<'a> {
-    Temporary(ImportedDataset),
-    Stored(&'a ImportedDataset),
-}
+#[derive(Debug)]
+struct NoDatasetImportedError;
 
-impl<'a> Deref for ImportedDatasetRef<'a> {
-    type Target = ImportedDataset;
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Self::Temporary(dataset) => &dataset,
-            Self::Stored(dataset) => dataset,
-        }
+impl fmt::Display for NoDatasetImportedError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "no dataset imported")
     }
 }
 
-enum CommandRunner {
-    ProgramArguments,
-    Interactive { dataset: Option<ImportedDataset> },
+impl Error for NoDatasetImportedError {}
+
+struct CommandRunner {
+    dataset: Option<ImportedDataset>,
 }
 
 impl CommandRunner {
-    fn program_arguments() -> Self {
-        Self::ProgramArguments
+    fn new() -> Self {
+        Self { dataset: None }
     }
 
-    fn interactive(dataset: Option<ImportedDataset>) -> Self {
-        Self::Interactive { dataset }
-    }
-
-    fn build_app(&self) -> clap::App<'static, 'static> {
-        let is_dataset_available = match self {
-            Self::ProgramArguments => false,
-            Self::Interactive { dataset } => dataset.is_some(),
-        };
+    fn build_app(&self, interactive: bool) -> clap::App<'static, 'static> {
         let app = clap_app!(gtfs_sim =>
             (@subcommand compress =>
                 (about: "Compresses a dataset into a single .bzip archive")
@@ -69,11 +55,11 @@ impl CommandRunner {
             )
             (@subcommand import =>
                 (about: "Imports a dataset")
-                (@arg dataset: <DATASET> "Path to gtfs dataset"))
+                (@setting TrailingVarArg)
+                (@arg dataset: <DATASET> "Path to gtfs dataset")
+                (@arg command: [COMMAND] +last +multiple "Command to run afterwards"))
             (@subcommand inspect =>
-                (about: "Inspects the dataset")
-                (@arg dataset: --dataset [DATASET] required(!is_dataset_available)
-                    "Path to gtfs dataset")
+                (about: "Inspects the imported dataset")
                 (@arg agency_name: --agency [AGENCY] "Filter after agency name")
                 (@arg line_name: <LINE> "Line name to inspect")
                 (@arg format: --format [FORMAT] +case_insensitive
@@ -82,9 +68,7 @@ impl CommandRunner {
                 (@arg output: --output [FILE] "Path to output file")
             )
             (@subcommand store =>
-                (about: "Generates a binary export")
-                (@arg dataset: --dataset [DATASET] required(!is_dataset_available)
-                    "Path to gtfs dataset")
+                (about: "Generates a binary export from the imported dataset")
                 (@arg profile: --profile [PROFILE]
                     possible_values(PROFILE_NAMES) default_value(DEFAULT_PROFILE_NAME)
                     "Profile used for exporting")
@@ -94,11 +78,11 @@ impl CommandRunner {
                 (about: "Loads a binary export to check for possible errors")
                 (@arg binary: [BINARY] default_value("wasm/www/data.bin") "Path to stored data")));
 
-        match self {
-            Self::Interactive { .. } => app
-                .setting(clap::AppSettings::NoBinaryName)
-                .setting(clap::AppSettings::DisableVersion),
-            _ => app,
+        if interactive {
+            app.setting(clap::AppSettings::NoBinaryName)
+                .setting(clap::AppSettings::DisableVersion)
+        } else {
+            app
         }
     }
 
@@ -110,100 +94,78 @@ impl CommandRunner {
         path
     }
 
-    fn retrieve_dataset(
-        &self,
-        matches: &clap::ArgMatches,
-    ) -> Result<ImportedDatasetRef, Box<dyn Error>> {
-        if let Some(path) = matches.value_of_os("dataset") {
-            let dataset = ImportedDataset::import(path)?;
-            Ok(ImportedDatasetRef::Temporary(dataset))
-        } else {
-            match self {
-                Self::Interactive {
-                    dataset: Some(dataset),
-                } => Ok(ImportedDatasetRef::Stored(&dataset)),
-                _ => unreachable!(),
-            }
+    fn dataset(&self) -> Result<&ImportedDataset, NoDatasetImportedError> {
+        self.dataset.as_ref().ok_or(NoDatasetImportedError)
+    }
+
+    fn run_with_program_arguments(&mut self) {
+        let app = self.build_app(false);
+        let matches = app.get_matches();
+        let should_run_interactively = self.execute_or_print(matches);
+        if should_run_interactively {
+            self.run_interactively();
         }
     }
 
-    fn run(&mut self) {
-        match self {
-            Self::ProgramArguments => {
-                let app = self.build_app();
-                let matches = app.get_matches();
-                self.execute(matches)
-                    .unwrap_or_else(|error| println!("{}", error));
-            }
-            Self::Interactive { .. } => {
-                let mut editor = Editor::<()>::new();
-                let _ = editor.load_history(&Self::history_path());
-                loop {
-                    let app = self.build_app();
-                    let line = match editor.readline("> ") {
-                        Ok(line) => line,
-                        Err(_) => break,
-                    };
-                    let arguments = match shlex::split(&line) {
-                        Some(arguments) => arguments,
-                        None => {
-                            println!("Erroneous input");
-                            continue;
-                        }
-                    };
-                    let matches = app.get_matches_from_safe(arguments);
-                    match matches {
-                        Ok(matches) => {
-                            editor.add_history_entry(line);
-                            self.execute(matches)
-                                .unwrap_or_else(|error| println!("{}", error));
-                        }
-                        Err(error) => {
-                            println!("{}", error);
-                        }
-                    }
+    fn run_interactively(&mut self) {
+        let mut editor = Editor::<()>::new();
+        let _ = editor.load_history(&Self::history_path());
+        loop {
+            let app = self.build_app(true);
+            let line = match editor.readline("> ") {
+                Ok(line) => line,
+                Err(_) => break,
+            };
+            let arguments = match shlex::split(&line) {
+                Some(arguments) => arguments,
+                None => {
+                    println!("Erroneous input");
+                    continue;
                 }
-                let _ = editor.save_history(&Self::history_path());
+            };
+            let matches = app.get_matches_from_safe(arguments);
+            match matches {
+                Ok(matches) => {
+                    editor.add_history_entry(line);
+                    self.execute_or_print(matches);
+                }
+                Err(error) => {
+                    println!("{}", error);
+                }
             }
         }
+        let _ = editor.save_history(&Self::history_path());
     }
 
-    fn execute(&mut self, matches: clap::ArgMatches) -> Result<(), Box<dyn Error>> {
+    fn execute(&mut self, matches: clap::ArgMatches) -> Result<bool, Box<dyn Error>> {
         match matches.subcommand() {
             ("compress", Some(compress_matches)) => {
                 let directory = compress_matches.value_of_os("directory").unwrap();
                 let archive = compress_matches.value_of_os("archive").unwrap();
-                compress(directory, archive)
+                compress(directory, archive)?;
             }
             ("import", Some(import_matches)) => {
+                let command_matches = import_matches.values_of("command").map(|command| {
+                    let app = self.build_app(true);
+                    app.get_matches_from(command)
+                });
                 let path = import_matches.value_of_os("dataset").unwrap();
-                let imported_dataset = ImportedDataset::import(path)?;
-                match self {
-                    Self::ProgramArguments => {
-                        *self = Self::interactive(Some(imported_dataset));
-                        self.run();
-                    }
-                    Self::Interactive {
-                        ref mut dataset, ..
-                    } => {
-                        *dataset = Some(imported_dataset);
-                    }
+                self.dataset = Some(ImportedDataset::import(path)?);
+                if let Some(command_matches) = command_matches {
+                    return self.execute(command_matches);
                 }
-                Ok(())
+                return Ok(true);
             }
             ("store", Some(store_matches)) => {
                 let profile = store_matches.value_of("profile").unwrap().try_into()?;
                 let date_formatted = store_matches.value_of("date").unwrap();
                 let date = NaiveDate::parse_from_str(date_formatted, "%F")?;
-                let dataset = self.retrieve_dataset(store_matches)?;
-
                 let file = File::create("wasm/www/data.bin")?;
-                dataset.store_into(file, profile, date)?;
-                Ok(())
+                self.dataset()?.store_into(file, profile, date)?;
             }
             ("load", Some(load_matches)) => {
                 let binary = load_matches.value_of_os("binary").unwrap();
-                load(binary)
+                load(binary)?;
             }
             ("inspect", Some(inspect_matches)) => {
                 let line_name = inspect_matches.value_of("line_name").unwrap();
@@ -214,24 +176,22 @@ impl CommandRunner {
                     Some(path) => Box::new(File::create(path)?),
                     None => Box::new(io::stdout()),
                 };
-                let dataset = self.retrieve_dataset(&inspect_matches)?;
-                inspect(&dataset, agency_name, line_name, format, &mut output)
+                inspect(self.dataset()?, agency_name, line_name, format, &mut output)?;
             }
-            ("", None) => {
-                match self {
-                    Self::ProgramArguments => {
-                        *self = Self::interactive(None);
-                        self.run();
-                    }
-                    Self::Interactive { .. } => {}
-                }
-                Ok(())
-            }
+            ("", None) => return Ok(true),
             _ => unreachable!(),
         }
+        Ok(false)
+    }
+
+    fn execute_or_print(&mut self, matches: clap::ArgMatches) -> bool {
+        self.execute(matches).unwrap_or_else(|error| {
+            println!("{}", error);
+            true
+        })
     }
 }
 
 fn main() {
-    CommandRunner::program_arguments().run();
+    CommandRunner::new().run_with_program_arguments();
 }
